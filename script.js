139b4aaa185d7c5a5f2231ed8951517888525611
WaelAlturi <101098027+WaelAlturi@users.noreply.github.com>
2024-02-14 13:03:33 +0200

    fix bug

--- script.js
+++ script.js
@@ -9,19 +9,13 @@ let previousNumber = "";
 let operator = "";
 
 function add(firstNumber, lastNumber) {
-  if (firstNumber.includes(".") || lastNumber.includes(".")) {
-    return parseFloat(firstNumber) + parseFloat(lastNumber);
-  }
-  return parseInt(firstNumber) + parseInt(lastNumber);
+  return parseFloat(firstNumber) + parseFloat(lastNumber);
 }
 function subtract(firstNumber, lastNumber) {
   return parseFloat(firstNumber) - parseFloat(lastNumber);
 }
 function multiply(firstNumber, lastNumber) {
-  if (firstNumber.includes(".") || lastNumber.includes(".")) {
-    return parseFloat(firstNumber) * parseFloat(lastNumber);
-  }
-  return parseInt(firstNumber) * parseInt(lastNumber);
+  return parseFloat(firstNumber) * parseFloat(lastNumber);
 }
 function divide(firstNumber, lastNumber) {
   return parseFloat(firstNumber) / parseFloat(lastNumber);
@@ -30,19 +24,21 @@ function divide(firstNumber, lastNumber) {
 function operate(operator, currentNumber, previousNumber) {
   switch (operator) {
     case "+":
-      return add(previousNumber, currentNumber);
+      return add(previousNumber, currentNumber).toString().includes(".")
+        ? add(previousNumber, currentNumber).toFixed(3)
+        : add(previousNumber, currentNumber);
     case "-":
-      return typeof subtract(previousNumber / currentNumber) === "number" &&
-        Number.isInteger(previousNumber / currentNumber)
-        ? previousNumber / currentNumber
-        : (previousNumber / currentNumber).toFixed(3);
+      return subtract(previousNumber, currentNumber).toString().includes(".")
+        ? subtract(previousNumber, currentNumber).toFixed(3)
+        : subtract(previousNumber, currentNumber);
     case "*":
-      return multiply(previousNumber, currentNumber);
+      return multiply(previousNumber, currentNumber).toString().includes(".")
+        ? multiply(previousNumber, currentNumber).toFixed(3)
+        : multiply(previousNumber, currentNumber);
     case "/":
-      return typeof divide(previousNumber / currentNumber) === "number" &&
-        Number.isInteger(previousNumber / currentNumber)
-        ? previousNumber / currentNumber
-        : (previousNumber / currentNumber).toFixed(3);
+      return divide(previousNumber, currentNumber).toString().includes(".")
+        ? divide(previousNumber, currentNumber).toFixed(3)
+        : divide(previousNumber, currentNumber);
     default:
       return currentNumber;
   }
@@ -52,7 +48,6 @@ numbers.forEach((button) => {
   button.addEventListener("click", () => {
     currentNumber += button.innerHTML;
     updateScreen();
-    console.log(typeof currentNumber);
   });
 });
 
@@ -60,7 +55,6 @@ operators.forEach((button) => {
   button.addEventListener("click", () => {
     if (previousNumber != "" && currentNumber != "") {
       currentNumber = operate(operator, currentNumber, previousNumber);
-      console.log(typeof currentNumber + "HELLO");
       updateScreen();
     }
     if (currentNumber !== "") {
@@ -93,12 +87,11 @@ decimal.addEventListener("click", () => {
 });
 remove.addEventListener("click", () => {
   if (currentNumber != "") {
-    currentNumber = currentNumber.slice(0, -1);
+    currentNumber = currentNumber.toString().slice(0, -1);
     updateScreen();
   }
   if (currentNumber === "") {
     display.innerHTML = "0";
-    console.log(display.innerHTML);
     clearScreen();
   }
 });
